f26a2fbf7c0646ca3835903328ff7dd2a05cd44b
Khalil Bouzidi <kbouziditn@gmail.com>
2024-07-10 17:58:56 +0100

    Added methods to checks if a workflow passes or failed

--- src/Traits/TracksActionStates.php
+++ src/Traits/TracksActionStates.php
@@ -2,6 +2,10 @@
 
 namespace Safemood\Workflow\Traits;
 
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Queue\Events\JobFailed;
+use Illuminate\Queue\Events\JobProcessed;
+use Queue;
 use Safemood\Workflow\Action;
 use Safemood\Workflow\Enums\ActionState;
 
@@ -37,5 +41,35 @@ trait TracksActionStates
             $action->setState($state);
             $action->setException($exception);
         }
+    }
+
+    
+    public function passes(): bool
+    {
+        $allActions = array_merge($this->beforeActions, $this->mainActions, $this->afterActions);
+
+        foreach ($allActions as $action) {
+            if (
+                $action instanceof Action
+                && $action->getState() !== ActionState::SUCCESS
+            ) {
+                return false;
+            }
+
+            if (
+                $action instanceof ShouldQueue && $action instanceof JobFailed
+            ) {
+                return false;
+            }
+
+        }
+
+        return true;
+    }
+
+
+    public function failed(): bool
+    {
+        return !$this->passes();
     }
 }
