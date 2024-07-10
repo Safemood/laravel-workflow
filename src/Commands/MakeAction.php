fd260a8c778d262d74324f2df274a42be49310e4
Khalil Bouzidi <kbouziditn@gmail.com>
2024-07-10 18:08:19 +0100

    Added a command to generate an action class

---
+++ src/Commands/MakeAction.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Safemood\Workflow\Commands;
+
+use Illuminate\Console\GeneratorCommand;
+use Symfony\Component\Console\Input\InputArgument;
+
+class MakeAction extends GeneratorCommand
+{
+    protected $name = 'make:action';
+    protected $description = 'Create a new action class';
+
+    protected $type = 'Action';
+
+    protected function getStub(): string
+    {
+        return file_exists($customPath = $this->laravel->basePath('stubs/action.stub'))
+            ? $customPath
+            : __DIR__.'/../../stubs/action.stub';
+    }
+
+    protected function getDefaultNamespace($rootNamespace): string
+    {
+        return "{$rootNamespace}\\Actions";
+    }
+
+    protected function getArguments(): array
+    {
+        return [
+            ['name', InputArgument::REQUIRED, 'The name of the action class.'],
+        ];
+    }
+}
