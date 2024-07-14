1c357549baa3fde247547e236de02e535417cc5b
Khalil Bouzidi <kbouziditn@gmail.com>
2024-07-14 18:45:22 +0100

    Added WorflowTest

---
+++ tests/feature/WorkflowTest.php
@@ -0,0 +1,56 @@
+<?php
+
+use App\Workflows\DummyWorkflow;
+use Tests\Helpers\DummyJob;
+use Tests\Helpers\DummyModel;
+use Tests\Helpers\DummyModelObserver;
+
+it('executes the DummyWorkflow correctly with events tracking and observers registrations', function () {
+
+    $context = [];
+
+    Queue::fake();
+
+    $workflow = (new DummyWorkflow())->run($context);
+
+    expect($workflow->passes())->toBeTrue();
+
+    Queue::assertPushed(DummyJob::class);
+
+    $trackedEvents = $workflow->trackedEvents();
+
+    expect($trackedEvents)->toHaveCount(8);
+    expect($trackedEvents[0]['event'])->toBe('App\Events\TestEvent');
+    expect($trackedEvents[1]['event'])->toBe('App\Events\AnotherEvent');
+    expect($trackedEvents[2]['event'])->toBe('OtherEvent');
+    expect($trackedEvents[3]['event'])->toBe('Illuminate\Log\Events\MessageLogged');
+    expect($trackedEvents[4]['event'])->toBe('eloquent.booting: '.DummyModel::class);
+    expect($trackedEvents[5]['event'])->toBe('eloquent.booted: '.DummyModel::class);
+    expect($trackedEvents[6]['event'])->toBe('Illuminate\Log\Events\MessageLogged');
+    expect($trackedEvents[7]['event'])->toBe('eloquent.created: '.DummyModel::class);
+
+    $attached = $this->assertObserverIsAttachedToEvent(DummyModelObserver::class, 'eloquent.booted: '.DummyModel::class);
+
+    expect($attached)->toBeTrue();
+});
+
+it('execute the DummyWorkflow correctly, failing to track events and register observers ', function () {
+
+    $context = [];
+
+    Queue::fake();
+
+    $workflow = (new DummyWorkflow(false, false))->run($context);
+
+    expect($workflow->passes())->toBeTrue();
+
+    Queue::assertPushed(DummyJob::class);
+
+    $trackedEvents = $workflow->trackedEvents();
+
+    expect($trackedEvents)->toBeEmpty();
+
+    $attached = $this->assertObserverIsAttachedToEvent(DummyModelObserver::class, 'eloquent.booted: '.DummyModel::class);
+
+    expect($attached)->toBeFalse();
+});
