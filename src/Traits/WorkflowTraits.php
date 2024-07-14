b0afb3c3041518ed729752756d6ad16e1b4fd776
Safemood <Safemood@users.noreply.github.com>
2024-07-14 22:50:54 +0000

    Fix styling

--- src/Traits/WorkflowTraits.php
+++ src/Traits/WorkflowTraits.php
@@ -1,15 +1,16 @@
 <?php
 
 namespace Safemood\Workflow\Traits;
+
 use Illuminate\Support\Traits\Dumpable;
 
 trait WorkflowTraits
 {
     use ActionsTrait;
+    use Dumpable;
     use HasResponses;
     use ManagesExecution;
     use RegistersObservers;
     use TracksActionStates;
     use TracksEvents;
-    use Dumpable;
 }
